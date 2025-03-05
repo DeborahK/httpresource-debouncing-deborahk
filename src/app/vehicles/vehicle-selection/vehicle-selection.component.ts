ae2cf8e52acc4b61bb8004e9a2d5f4cc7b0863c0
DeborahK <git@stackblitz.com>
2025-03-05 14:26:51 -0800

    Initial commit

---
+++ src/app/vehicles/vehicle-selection/vehicle-selection.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { VehicleService } from '../vehicle.service';
+
+@Component({
+   selector: 'app-vehicle-selection',
+   imports: [FormsModule],
+   templateUrl: './vehicle-selection.component.html',
+   styleUrl: './vehicle-selection.component.css'
+})
+export class VehicleSelectionComponent {
+   pageTitle = "StarWars Vehicles";
+
+   // Injected services
+   private vehicleService = inject(VehicleService);
+
+   // Signals to support the template
+   vehicles = this.vehicleService.vehicles;
+   isLoading = this.vehicleService.isLoading;
+   errorMessage = this.vehicleService.errorMessage;
+   enteredModel = this.vehicleService.enteredModel;
+}
